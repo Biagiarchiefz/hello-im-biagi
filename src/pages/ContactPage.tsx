a27a46bf92a1c90156257521322155a0ec04ac06
Biagiarchiefz <biagiiarchie@gmail.com>
2025-10-14 09:22:19 +0700

    add contact title

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import contactMe from "../assets/img/Contact Me-b (1).png"
 
 const ContactPage = () => {
   return (
-    <div className='text-white'>
-      <h1>ini contact</h1>
+    <div className='text-white px-[160px] mt-20'>
+        <div className="title relative">
+          <h1 className="text-[#7E62F3] font-bold text-5xl relative pt-16 px-11 flex justify-center">
+            CONTACT ME
+          </h1>
+          <img
+            src={contactMe}
+            alt=""
+            className="w-[773px] absolute top-0 left-1/2 -translate-x-1/2"
+          />
+        </div>
     </div>
   )
 }
